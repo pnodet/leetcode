32d06d362f0a1096a886754e4c4b2367405a7924
pnodet <paul.nodet@gmail.com>
2013-11-18 17:22:16 +0100

    0001

---
+++ 0001.js
@@ -0,0 +1,11 @@
+const twoSum = (nums, target) => {
+	const map = {};
+
+	for (let i = 0; i < nums.length; i++) {
+		const complement = target - nums[i];
+		if (map[complement] !== undefined) {
+			return [map[complement], i];
+		}
+		map[nums[i]] = i;
+	}
+};
