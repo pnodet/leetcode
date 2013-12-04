1d6240342236e0c003c874e17bc0b01445f7efe0
pnodet <paul.nodet@gmail.com>
2013-12-04 17:43:12 +0100

    0020

---
+++ 0020.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isValid(s) {
+	const stack = [];
+	const mapping = {')': '(', '}': '{', ']': '['};
+
+	for (let char of s) {
+		if (char in mapping) {
+			const topElement = stack.length === 0 ? '#' : stack.pop();
+			if (mapping[char] !== topElement) {
+				return false;
+			}
+		} else {
+			stack.push(char);
+		}
+	}
+
+	return stack.length === 0;
+}
