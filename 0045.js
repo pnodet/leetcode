da66e54d691b9118a42ac2e0063c75ecf4443f46
pnodet <paul.nodet@gmail.com>
2014-01-22 17:25:46 +0100

    0045

---
+++ 0045.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function jump(nums) {
+	let jumps = 0;
+	let currentJumpEnd = 0;
+	let farthest = 0;
+
+	for (let i = 0; i < nums.length - 1; i++) {
+		farthest = Math.max(farthest, i + nums[i]);
+		if (i === currentJumpEnd) {
+			jumps++;
+			currentJumpEnd = farthest;
+		}
+	}
+
+	return jumps;
+}
