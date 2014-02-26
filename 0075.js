b8b09dc31822668e0662140568158ff7b0896f9b
pnodet <paul.nodet@gmail.com>
2014-02-26 17:20:49 +0100

    0075

---
+++ 0075.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums) {
+	let left = 0;
+	let right = nums.length - 1;
+	let index = 0;
+
+	while (index <= right) {
+		if (nums[index] === 0) {
+			[nums[index], nums[left]] = [nums[left], nums[index]];
+			left++;
+			index++;
+		} else if (nums[index] === 2) {
+			[nums[index], nums[right]] = [nums[right], nums[index]];
+			right--;
+		} else {
+			index++;
+		}
+	}
+}
