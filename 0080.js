6b12012d85218da2da2bfbc69988c4986bd93e86
pnodet <paul.nodet@gmail.com>
2014-03-03 17:57:26 +0100

    0080

---
+++ 0080.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {
+	if (nums.length <= 2) return nums.length;
+
+	let index = 2;
+
+	for (let i = 2; i < nums.length; i++) {
+		if (nums[i] !== nums[index - 2]) nums[index++] = nums[i];
+	}
+
+	return index;
+}
